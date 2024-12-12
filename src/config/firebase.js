ad30d1a85fbfceb866081f1ab41613ed24cb5785
Katerina Kolarova <kolarkaty@gmail.com>
2024-12-12 13:40:41 +0100

    removed logs in firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -13,9 +13,6 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Debug: Log the Firebase configuration to verify environment variables
-console.log("Firebase Config:", firebaseConfig);
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
