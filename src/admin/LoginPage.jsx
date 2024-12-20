f372f81bd04c2343f979d64896e24aa7af093cdd
Katerina Kolarova <kolarkaty@gmail.com>
2024-12-20 12:43:21 +0100

    login page styles

--- src/admin/LoginPage.jsx
+++ src/admin/LoginPage.jsx
@@ -1,19 +1,21 @@
-import { Stack } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import Auth from '../components/Auth';
 
 export default function LoginPage(){
 
     return (
-        <Stack
-            direction="column"
-            spacing={0}
-            sx={{
-            mt: 3,
-            justifyContent: "center",
-            alignItems: "center",
-            }}
-        >
-            <Auth/>
-        </Stack>
+        <Box sx={{ maxHeight: '100vh', padding: 3 }}>
+            <Stack
+                direction="column"
+                spacing={0}
+                sx={{
+                mt: 3,
+                justifyContent: "center",
+                alignItems: "center",
+                }}
+            >
+                <Auth/>
+            </Stack>
+        </Box>
     )
 }
